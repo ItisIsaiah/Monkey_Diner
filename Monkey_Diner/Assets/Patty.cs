5ea83dc3dde27c3282f03a2ceefa4ed60c386fd0
ZayX731 <zayfootballplayer@gmail.com>
2021-05-25 01:41:54 -0400

    Cooking burgers
    Now if you throw a patty on the grill it'll start cooking! also I made the teleport areas invisible so that the gray does not annoy anyone

---
+++ Monkey_Diner/Assets/Patty.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patty : MonoBehaviour
+{
+    Material material;
+    public bool isCooking = false;
+    public float cookedPercentage=0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        material = GetComponentInChildren<MeshRenderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Cooking();
+    }
+    void Cooking()
+    {
+        while (isCooking == true)
+        {
+            Debug.Log("WE got to this point");
+            float time = 0;
+            Debug.Log("IM COOKING");
+            time += Time.deltaTime / 10;
+            material.SetFloat("Color Value", time);
+        }
+    }
+}
+//
---
+++ Monkey_Diner/Assets/StoveCooking.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoveCooking : MonoBehaviour
+{
+    float time=0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        
+        Patty patty= other.GetComponent<Patty>();
+        patty.cookedPercentage = time * 100;
+        Material mat= other.GetComponentInChildren<MeshRenderer>().material;
+         
+        
+
+        Debug.Log(time);
+        time += Time.deltaTime/10;
+        mat.SetFloat("_ColorValue", time);
+        
+    }
+
+}
