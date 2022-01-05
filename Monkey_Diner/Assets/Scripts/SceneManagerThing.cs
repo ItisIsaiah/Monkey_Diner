692f03e1e14f045781a74815ea5ae1ee8aa49fd4
ZayX731 <zayfootballplayer@gmail.com>
2022-01-05 02:40:37 -0500

    Arcade
    Added an arcade feature with hopes that  as you beat certain diners, you get more games.

--- Monkey_Diner/Assets/Scripts/SceneManagerThing.cs
+++ Monkey_Diner/Assets/Scripts/SceneManagerThing.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class SceneManagerThing : MonoBehaviour
 {
+    public int sceneSelection { get; set; } = 0;
+
     // Start is called before the first frame update
     public void NextLevel()
     {
@@ -22,6 +24,13 @@ public class SceneManagerThing : MonoBehaviour
         SceneManager.LoadScene("Game Over");
     }
 
-    
+    public void LoadSelectedScene() {
+        SceneManager.LoadScene(sceneSelection);
+    }
 
+    
+    public void LoadSceneDropdown(int val)
+    {
+        SceneManager.LoadScene(val);
+    }
 }
